4684763059a78cef999e056b66881b822d107296
Akondu Okpe <78967468+nomadcomposer@users.noreply.github.com>
2025-12-16 17:18:02 +0100

    Complete full Knight Rider animation

--- KnightRider/KnightRider.ino
+++ KnightRider/KnightRider.ino
@@ -1,7 +1,7 @@
 int dataPin = 9;
 int latchPin = 10;
 int clockPin = 11;
-int dt = 250;
+int dt = 100;
 
 byte j;
 
@@ -16,6 +16,13 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   for (j=0x80;j>0x00;j/=0x02) {
+    digitalWrite(latchPin, LOW);
+    shiftOut(dataPin, clockPin, LSBFIRST, j);
+    digitalWrite(latchPin, HIGH);
+    delay(dt);
+  }
+
+  for (j=0x01;j<0x80;j*=0x02) {
     digitalWrite(latchPin, LOW);
     shiftOut(dataPin, clockPin, LSBFIRST, j);
     digitalWrite(latchPin, HIGH);
