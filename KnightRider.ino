7b21adefad063c37b0a8dbd39165acf1a6aa342a
Akondu Okpe <78967468+nomadcomposer@users.noreply.github.com>
2025-12-16 12:15:11 +0100

    Add files via upload

---
+++ KnightRider.ino
@@ -0,0 +1,10 @@
+int clockPin = 9;
+int latchPin = 10;
+int dataPin = 11;
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
