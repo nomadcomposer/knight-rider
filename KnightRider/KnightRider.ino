6edd52243b3cd00365acf6dd76b67c1c066fc26d
Akondu Okpe <78967468+nomadcomposer@users.noreply.github.com>
2025-12-16 16:46:34 +0100

    Implement sample LED pattern

--- KnightRider/KnightRider.ino
+++ KnightRider/KnightRider.ino
@@ -1,6 +1,8 @@
 int dataPin = 9;
 int latchPin = 10;
 int clockPin = 11;
+
+byte LED1s = 0x02;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -11,4 +13,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, LSBFIRST, LED1s);
+  digitalWrite(latchPin, HIGH);
+}
